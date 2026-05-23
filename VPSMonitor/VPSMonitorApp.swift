ebf410e1a1c6bb3e8280640f168490129d8ecf9d
lampardrodgers <sunjh42a3@163.com>
2026-05-23 23:47:14 +0800

    Initial Commit

---
+++ VPSMonitor/VPSMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPSMonitorApp.swift
+//  VPSMonitor
+//
+//  Created by MisakaMikoto on 2026/5/23.
+//
+
+import SwiftUI
+
+@main
+struct VPSMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
